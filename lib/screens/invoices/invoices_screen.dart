bdaaa342b7d09acbf3327617432be878d91d08e4
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 21:07:48 +0300

    Update invoices_screen.dart

--- lib/screens/invoices/invoices_screen.dart
+++ lib/screens/invoices/invoices_screen.dart
@@ -1,1 +1,45 @@
 
+import 'package:flutter/material.dart';
+
+
+class InvoicesScreen extends StatelessWidget {
+
+
+const InvoicesScreen({super.key});
+
+
+@override
+Widget build(BuildContext context){
+
+
+return Scaffold(
+
+appBar:AppBar(
+
+title:
+const Text("الفواتير")
+
+),
+
+
+body:
+
+const Center(
+
+child:
+
+Text(
+
+"إدارة الفواتير"
+
+),
+
+),
+
+
+);
+
+
+}
+
+}

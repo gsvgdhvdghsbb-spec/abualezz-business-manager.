d44e283031ee919eacc29366af79258dc0e75cb3
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 22:02:32 +0300

    Update reports_screen.dart

--- lib/screens/reports/reports_screen.dart
+++ lib/screens/reports/reports_screen.dart
@@ -1,1 +1,59 @@
+import 'package:flutter/material.dart';
 
+
+class ReportsScreen extends StatelessWidget{
+
+
+const ReportsScreen({super.key});
+
+
+
+@override
+Widget build(BuildContext context){
+
+
+return Scaffold(
+
+appBar:
+
+AppBar(
+
+title:
+const Text("التقارير")
+
+),
+
+
+body:
+
+const Center(
+
+child:
+
+Text(
+
+"تقارير المبيعات والأرباح"
+
+,
+
+style:
+
+TextStyle(
+
+fontSize:20,
+
+fontWeight:FontWeight.bold
+
+)
+
+)
+
+)
+
+);
+
+
+}
+
+
+}

a604f02f2390661345a1b13ebf9dd78d0dc85a6b
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 21:02:32 +0300

    Update database_helper.dart

--- lib/database/database_helper.dart
+++ lib/database/database_helper.dart
@@ -1,1 +1,99 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
 
+
+
+class DatabaseHelper{
+
+
+static Database? _database;
+
+
+
+Future<Database> get database async{
+
+
+if(_database != null){
+
+return _database!;
+
+}
+
+
+_database = await initDatabase();
+
+
+return _database!;
+
+
+}
+
+
+
+Future<Database> initDatabase() async{
+
+
+String path = join(
+
+await getDatabasesPath(),
+
+"abu_al_ezz.db"
+
+);
+
+
+
+return await openDatabase(
+
+path,
+
+version:1,
+
+onCreate:(db,version) async{
+
+
+await db.execute('''
+
+CREATE TABLE customers(
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+name TEXT,
+
+phone TEXT,
+
+debt REAL
+
+)
+
+''');
+
+
+
+await db.execute('''
+
+CREATE TABLE invoices(
+
+id INTEGER PRIMARY KEY AUTOINCREMENT,
+
+customer TEXT,
+
+total REAL,
+
+date TEXT
+
+)
+
+''');
+
+
+},
+
+);
+
+
+}
+
+
+
+}

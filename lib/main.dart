ca8dc54ce019930cee6bc16ebaaca1debaaae1dc
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 20:32:02 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,24 @@
+import 'package:flutter/material.dart';
+import 'screens/auth/login_screen.dart';
 
+void main() {
+  runApp(const AbuAlEzzApp());
+}
+
+class AbuAlEzzApp extends StatelessWidget {
+  const AbuAlEzzApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Abu Al-Ezz Business Manager',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Cairo',
+        useMaterial3: true,
+      ),
+      home: const LoginScreen(),
+    );
+  }
+}

4e88fcd65f713a5f9c444c763300948a5aee9bea
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 21:00:40 +0300

    Update customer.dart

--- lib/models/customer.dart
+++ lib/models/customer.dart
@@ -1,1 +1,63 @@
 
+class CustomerModel {
+
+
+  int? id;
+
+  String name;
+
+  String phone;
+
+  double debt;
+
+
+  CustomerModel({
+
+    this.id,
+
+    required this.name,
+
+    required this.phone,
+
+    required this.debt,
+
+  });
+
+
+
+  Map<String,dynamic> toMap(){
+
+    return {
+
+      "id": id,
+
+      "name": name,
+
+      "phone": phone,
+
+      "debt": debt,
+
+    };
+
+  }
+
+
+
+  factory CustomerModel.fromMap(
+      Map<String,dynamic> map){
+
+    return CustomerModel(
+
+      id: map["id"],
+
+      name: map["name"],
+
+      phone: map["phone"],
+
+      debt: map["debt"],
+
+    );
+
+  }
+
+}

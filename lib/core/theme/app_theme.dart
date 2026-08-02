dcf37eb64a5c6e07d37663046ce94d689ead19f8
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 20:59:10 +0300

    Update app_theme.dart

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,1 +1,27 @@
 
+import 'package:flutter/material.dart';
+
+
+class AppColors {
+
+
+  static const Color primary =
+      Color(0xff0D1B2A);
+
+
+  static const Color secondary =
+      Color(0xff1565C0);
+
+
+  static const Color background =
+      Color(0xffF5F7FA);
+
+
+  static const Color success =
+      Colors.green;
+
+
+  static const Color danger =
+      Colors.red;
+
+}

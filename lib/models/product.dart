363658e2fb0fb9ce333b17bfaf559e6ad239f133
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 21:15:20 +0300

    Update product.dart

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,1 +1,63 @@
+class ProductModel {
 
+
+int? id;
+
+String name;
+
+double price;
+
+int quantity;
+
+
+
+ProductModel({
+
+this.id,
+
+required this.name,
+
+required this.price,
+
+required this.quantity,
+
+});
+
+
+
+Map<String,dynamic> toMap(){
+
+return{
+
+"id":id,
+
+"name":name,
+
+"price":price,
+
+"quantity":quantity,
+
+};
+
+}
+
+
+factory ProductModel.fromMap(
+Map<String,dynamic> map){
+
+return ProductModel(
+
+id:map["id"],
+
+name:map["name"],
+
+price:map["price"],
+
+quantity:map["quantity"],
+
+);
+
+}
+
+
+}

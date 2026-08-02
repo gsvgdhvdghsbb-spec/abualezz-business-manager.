8b5c3c3707d89ac91d03e669ed9f1de4c40167cb
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 22:52:03 +0300

    Update backup_service.dart

--- lib/services/backup_service.dart
+++ lib/services/backup_service.dart
@@ -1,1 +1,109 @@
+import 'dart:io';
 
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+
+import '../database/database_helper.dart';
+
+
+
+class BackupService {
+
+
+  final dbHelper =
+  DatabaseHelper();
+
+
+
+
+  Future<String> createBackup() async {
+
+
+
+    Directory directory =
+    await getApplicationDocumentsDirectory();
+
+
+
+    String databasePath =
+    join(
+
+      await getDatabasesPath(),
+
+      "abu_al_ezz.db",
+
+    );
+
+
+
+    File databaseFile =
+    File(databasePath);
+
+
+
+    String backupPath =
+    join(
+
+      directory.path,
+
+      "abu_al_ezz_backup.db",
+
+    );
+
+
+
+    await databaseFile.copy(
+
+      backupPath,
+
+    );
+
+
+
+    return backupPath;
+
+
+
+  }
+
+
+
+
+
+
+  Future<void> restoreBackup(
+
+      String path
+
+      ) async {
+
+
+
+    String databasePath =
+    join(
+
+      await getDatabasesPath(),
+
+      "abu_al_ezz.db",
+
+    );
+
+
+
+    File backupFile =
+    File(path);
+
+
+
+    await backupFile.copy(
+
+      databasePath,
+
+    );
+
+
+
+  }
+
+
+}

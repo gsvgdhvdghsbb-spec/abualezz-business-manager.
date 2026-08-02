990ab7ea147dbe4afff572642ff0ff00e6d37cbb
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 21:06:40 +0300

    Update suppliers_screen.dart

--- lib/screens/suppliers/suppliers_screen.dart
+++ lib/screens/suppliers/suppliers_screen.dart
@@ -1,1 +1,44 @@
 
+import 'package:flutter/material.dart';
+
+
+class SuppliersScreen extends StatelessWidget {
+
+
+const SuppliersScreen({super.key});
+
+
+@override
+Widget build(BuildContext context){
+
+
+return Scaffold(
+
+appBar:AppBar(
+
+title:
+const Text("الموردون")
+
+),
+
+
+body:
+const Center(
+
+child:
+Text(
+
+"صفحة الموردين"
+
+),
+
+),
+
+
+);
+
+
+}
+
+
+}

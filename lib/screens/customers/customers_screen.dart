71ec844b092a8c3bf0a7f7c74e3ab27622c71f84
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 21:13:44 +0300

    Update customers_screen.dart

--- lib/screens/customers/customers_screen.dart
+++ lib/screens/customers/customers_screen.dart
@@ -1,10 +1,94 @@
-
 import 'package:flutter/material.dart';
+import '../../models/customer_model.dart';
+import '../../services/database_service.dart';
 
 
-class CustomersScreen extends StatelessWidget {
+class CustomersScreen extends StatefulWidget {
 
 const CustomersScreen({super.key});
+
+
+@override
+State<CustomersScreen> createState()
+=> _CustomersScreenState();
+
+}
+
+
+
+class _CustomersScreenState 
+extends State<CustomersScreen>{
+
+
+final service = DatabaseService();
+
+
+List<CustomerModel> customers=[];
+
+
+final name =
+TextEditingController();
+
+
+final phone =
+TextEditingController();
+
+
+
+@override
+void initState(){
+
+super.initState();
+
+loadCustomers();
+
+}
+
+
+
+void loadCustomers() async {
+
+
+customers =
+await service.getCustomers();
+
+
+setState((){});
+
+
+}
+
+
+
+void addCustomer() async{
+
+
+await service.addCustomer(
+
+CustomerModel(
+
+name:name.text,
+
+phone:phone.text,
+
+debt:0,
+
+)
+
+);
+
+
+name.clear();
+
+phone.clear();
+
+
+loadCustomers();
+
+
+}
+
+
 
 
 @override
@@ -13,23 +97,149 @@ Widget build(BuildContext context){
 
 return Scaffold(
 
+
 appBar:AppBar(
 
 title:
-const Text("العملاء")
+const Text("العملاء"),
 
 ),
 
 
-body:Center(
+floatingActionButton:
 
-child:ElevatedButton(
+FloatingActionButton(
 
 child:
-const Text("إضافة عميل"),
+const Icon(Icons.add),
 
 onPressed:(){
 
+showDialog(
+
+context:context,
+
+builder:(context){
+
+return AlertDialog(
+
+title:
+const Text("إضافة عميل"),
+
+
+content:
+
+Column(
+
+mainAxisSize:
+MainAxisSize.min,
+
+children:[
+
+
+TextField(
+
+controller:name,
+
+decoration:
+const InputDecoration(
+labelText:"اسم العميل"
+),
+
+),
+
+
+TextField(
+
+controller:phone,
+
+decoration:
+const InputDecoration(
+labelText:"رقم الهاتف"
+),
+
+),
+
+
+],
+
+),
+
+
+actions:[
+
+TextButton(
+
+onPressed:(){
+
+addCustomer();
+
+Navigator.pop(context);
+
+},
+
+child:
+const Text("حفظ"),
+
+)
+
+],
+
+);
+
+
+}
+
+);
+
+
+},
+
+
+),
+
+
+
+body:
+
+ListView.builder(
+
+itemCount:
+customers.length,
+
+itemBuilder:(context,index){
+
+
+final c =
+customers[index];
+
+
+return Card(
+
+child:ListTile(
+
+title:
+Text(c.name),
+
+
+subtitle:
+Text(
+"الهاتف: ${c.phone}\nالدين: ${c.debt}"
+),
+
+
+trailing:
+
+IconButton(
+
+icon:
+const Icon(Icons.delete),
+
+onPressed:(){
+
+service.deleteCustomer(c.id!);
+
+loadCustomers();
 
 },
 
@@ -38,11 +248,19 @@ onPressed:(){
 
 ),
 
+);
+
+
+},
+
+
+),
+
+
 
 );
 
 
 }
-
 
 }

cb2c04c598f15bb9cb9626ac77dc6210cb057e15
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 20:57:31 +0300

    Update app_constants.dart

--- lib/core/constants/app_constants.dart
+++ lib/core/constants/app_constants.dart
@@ -1,1 +1,13 @@
+class AppConstants {
 
+  static const String appName =
+      "محاسبة أبو العز";
+
+  static const String appEnglishName =
+      "Abu Al-Ezz Business Manager";
+
+
+  static const String currency =
+      "ريال يمني";
+
+}

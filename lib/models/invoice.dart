d9f919a64fa802ca31f1902ffe16ffc628448109
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 21:01:22 +0300

    Update invoice.dart

--- lib/models/invoice.dart
+++ lib/models/invoice.dart
@@ -1,1 +1,49 @@
 
+class InvoiceModel {
+
+
+  int? id;
+
+
+  String customer;
+
+
+  double total;
+
+
+  String date;
+
+
+
+  InvoiceModel({
+
+    this.id,
+
+    required this.customer,
+
+    required this.total,
+
+    required this.date,
+
+  });
+
+
+
+  Map<String,dynamic> toMap(){
+
+    return {
+
+      "id":id,
+
+      "customer":customer,
+
+      "total":total,
+
+      "date":date,
+
+    };
+
+  }
+
+
+}

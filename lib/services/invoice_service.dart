5bbac0fdf91a9260e532b46d5fbe79d7d240ea60
gsvgdhvdghsbb-spec <gsvgdhvdghsbb@gmail.com>
2026-08-02 22:28:19 +0300

    Update invoice_service.dart

--- lib/services/invoice_service.dart
+++ lib/services/invoice_service.dart
@@ -1,49 +1,193 @@
 import '../database/database_helper.dart';
 import '../models/invoice_model.dart';
+import '../models/invoice_item_model.dart';
+
 
 
 class InvoiceService {
 
 
-final db = DatabaseHelper();
+  final db = DatabaseHelper();
 
 
 
-Future<int> addInvoice(InvoiceModel invoice) async{
 
 
-final database =
-await db.database;
+  Future<int> saveInvoice(
 
+      InvoiceModel invoice,
 
-return await database.insert(
-"invoices",
-invoice.toMap()
-);
+      List<InvoiceItemModel> items,
 
-
-}
+      ) async {
 
 
 
-Future<List<InvoiceModel>> getInvoices() async{
+    final database =
+    await db.database;
 
 
-final database =
-await db.database;
+
+    int invoiceId =
+    await database.insert(
+
+      "invoices",
+
+      invoice.toMap(),
+
+    );
 
 
-final data =
-await database.query("invoices");
 
 
-return data
-.map((e)=>InvoiceModel.fromMap(e))
-.toList();
+
+    for(var item in items){
 
 
-}
+      await database.insert(
 
+        "invoice_items",
+
+        {
+
+          "invoiceId": invoiceId,
+
+          "productName":
+          item.productName,
+
+          "price":
+          item.price,
+
+          "quantity":
+          item.quantity,
+
+          "total":
+          item.total,
+
+        },
+
+      );
+
+
+
+      await database.rawUpdate(
+
+        '''
+
+        UPDATE products
+
+        SET quantity = quantity - ?
+
+        WHERE name = ?
+
+        ''',
+
+        [
+
+          item.quantity,
+
+          item.productName,
+
+        ],
+
+      );
+
+
+
+    }
+
+
+
+
+    return invoiceId;
+
+
+
+  }
+
+
+
+
+
+
+  Future<List<InvoiceModel>> getInvoices() async{
+
+
+    final database =
+    await db.database;
+
+
+
+    final data =
+    await database.query(
+
+      "invoices",
+
+      orderBy:
+      "id DESC",
+
+    );
+
+
+
+    return data
+
+        .map(
+
+          (e)=>InvoiceModel.fromMap(e),
+
+    )
+
+        .toList();
+
+
+  }
+
+
+
+
+
+
+
+  Future<List<InvoiceItemModel>> getInvoiceItems(
+
+      int invoiceId
+
+      ) async{
+
+
+    final database =
+    await db.database;
+
+
+
+    final data =
+    await database.query(
+
+      "invoice_items",
+
+      where:
+      "invoiceId=?",
+
+      whereArgs:
+      [invoiceId],
+
+    );
+
+
+
+    return data
+
+        .map(
+
+          (e)=>InvoiceItemModel.fromMap(e),
+
+    )
+
+        .toList();
+
+
+  }
 
 
 }
